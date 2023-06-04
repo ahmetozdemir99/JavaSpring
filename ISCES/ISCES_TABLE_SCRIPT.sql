a4e67a00179ec413919cc1248aa2e2a3fc0d7219
ahmetozdemir99 <ahmetozdemir99@hotmail.com>
2023-06-05 00:36:53 +0300

    .

--- ISCES/ISCES_TABLE_SCRIPT.sql
+++ ISCES/ISCES_TABLE_SCRIPT.sql
@@ -53,8 +53,9 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `election` (
   `election_id` BIGINT NOT NULL,
-  `start_date` DATE NOT NULL,
-  `is_completed` DATE NOT NULL,
+  `start_date` DATETIME NOT NULL,
+  `end_date` DATETIME NOT NULL,
+  `is_finished` TINYINT,
   PRIMARY KEY (`election_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
